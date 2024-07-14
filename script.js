5eaacc279050623aaf960833a08334dba024e4ab
ShesAMood <90805140+ShesAMood@users.noreply.github.com>
2024-07-14 04:48:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function calculate() {
+    const magicLevel = parseInt(document.getElementById('magicLevel').value);
+    const useVoid = document.getElementById('useVoid').checked;
+    
+    let boostType = document.querySelector('input[name="boostType"]:checked').value;
+    let prayerType = document.querySelector('input[name="prayerType"]:checked').value;
+
+    let toBeat = (magicLevel + 9) * 204;
+    let visual;
+
+    if (boostType == 3) {
+        visual = magicLevel; // No boost
+    } else if (boostType == 1) {
+        visual = (magicLevel * 1.1) + 4; // Saturated Heart
+    } else if (boostType == 2) {
+        visual = magicLevel + 4; // Divine Mage Pot
+    }
+
+    let effective = Math.floor(visual);
+
+    if (prayerType == 3) {
+        effective *= 1; // No Prayer
+    } else if (prayerType == 2) {
+        effective *= 1.15; // Mystic Might
+    } else if (prayerType == 1) {
+        effective *= 1.25; // Augury
+    }
+
+    effective = Math.floor(effective);
+
+    if (useVoid) {
+        effective *= 1.45;
+        effective = Math.floor(effective);
+    }
+
+    effective += 9;
+
+    const result = Math.ceil((toBeat / effective) - 64);
+
+    document.getElementById('result').innerText = `You need a magic attack bonus of: ${result}`;
+}
+
+function exitProgram() {
+    window.close();
+}
